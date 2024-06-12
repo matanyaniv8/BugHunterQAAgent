4d4658e6287d0b051dd09d54776fa966f4d75a62
Shahar Hahami <hahami.shahar@gmail.com>
2024-06-12 15:14:11 +0300

    move tests results to unified file. need to fix buttons and form

--- QA_Agent/agent.py
+++ QA_Agent/agent.py
@@ -1,53 +1,66 @@
+import os
 from tests.button_tests import run_tests_html_code as button_tests_html, run_url_tests as button_tests_url
 from tests.link_tests import run_link_tests_html_code as link_tests_html, run_url_link_tests as link_tests_url
 from tests.form_tests import run_form_tests
+from urllib.parse import urlparse
 
 
-def run_tests_wrapper(web_data, test_type="button"):
+def run_tests_wrapper(web_data, filename):
     """
-    Wrapper function for run_tests, if the website is an HTML code or an URL.
-    Writes the results into a file.
+    Wrapper function for running tests on buttons, links, and forms.
+    Writes the results into a single file.
     :param web_data: HTML code or URL.
-    :param test_type: Type of test to run - "button" or "link".
+    :param filename: Name of the file to write to.
     """
-    if test_type == "button":
-        if web_data.startswith("http"):
-            results, filename = button_tests_url(web_data)
-        else:
-            results, filename = button_tests_html(web_data)
-    elif test_type == "link":
-        if web_data.startswith("http"):
-            results, filename = link_tests_url(web_data)
-        else:
-            results, filename = link_tests_html(web_data)
-    elif test_type == "form":
-        # results, filename = (
-            run_form_tests(web_data)
+    # Remove file if it exists
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    is_url = web_data.startswith("http")
+
+    # Run button tests
+    if is_url:
+        #button_results, _ = button_tests_url(web_data)
+        button_results = []
+        link_results, _ = link_tests_url(web_data)
+        #form_results = run_form_tests(web_data)
+        #write_results_to_file(button_results, filename, "form")
     else:
-        print(f"Unknown test type: {test_type}")
-        return
+        button_results, _ = button_tests_html(web_data)
+        link_results, _ = link_tests_url(web_data)
 
-    # write_results_to_file(results, filename, test_type)
-
+    form_results = []  # todo - form is missing here
+    write_results_to_file(button_results, filename, "button")
+    write_results_to_file(link_results, filename, "link")
+    write_results_to_file(form_results, filename, "form")
 
 def write_results_to_file(results, filename, test_type):
     """
     Writes the test results to a file.
     :param results: Test results.
     :param filename: Name of the file to write to.
-    :param test_type: Type of test - "button" or "link".
+    :param test_type: Type of test - "button", "link", or "form".
     """
-    with open(filename, "w") as file:
+    with open(filename, "a") as file:
+        file.write(f"########## {test_type.upper()} TESTS ##########\n\n")
+
+        if not results:
+            file.write("No results found.\n\n")
+            return
+
         for result in results:
-            file.write(f"### {result['name']} ###\n")
+            file.write(f"### {result.get('name', 'Unnamed Test')} ###\n")
             if test_type == "button":
                 file.write(f"Button Text: {result.get('button_text', 'N/A')}\n")
                 file.write(f"Result: {result.get('outcome', 'N/A')}\n")
-            else:
+            elif test_type == "link":
                 file.write(f"Link Text: {result.get('link_text', 'N/A')}\n")
                 file.write(f"Link Href: {result.get('link_href', 'N/A')}\n")
                 for test, outcome in result.get('outcomes', {}).items():
                     file.write(f"    {test}: {outcome}\n")
+            elif test_type == "form":
+                file.write(f"Form Text: {result.get('form_text', 'N/A')}\n")
+                file.write(f"Result: {result.get('outcome', 'N/A')}\n")
             file.write(f"Overall Result: {result.get('overall_outcome', 'N/A')}\n\n")
 
 
@@ -67,15 +80,24 @@ def get_html_content(file_path):
     return None
 
 
+def get_domain_from_url(url):
+    """
+    Extracts the domain name from a URL.
+    :param url: URL to extract the domain from.
+    :return: Domain name.
+    """
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    return domain
+
+
+
 if __name__ == "__main__":
     web_path = '../Websites_Generator/generated_html/buggy_website.html'
     html_content = get_html_content(web_path)
     if html_content:
-        # run_tests_wrapper(html_content, test_type="button")
-        run_tests_wrapper(html_content, test_type="link")
+        #run_tests_wrapper(html_content, filename={f"websitesTestResult/buggy_website_tests.html"})
+        pass
 
-    # url = 'https://themeforest.net/search/dummy'
     url = "https://www.mako.co.il/collab/N12_Contact.html?partner=NewsfooterLinks&click_id=esDU5sDbdL"
-    # run_tests_wrapper(url, test_type="button")
-    # run_tests_wrapper(url, test_type="link")
-    run_tests_wrapper(url, test_type="form")
+    run_tests_wrapper(url, f"websitesTestsResult/{get_domain_from_url(url)}_tests.text")
