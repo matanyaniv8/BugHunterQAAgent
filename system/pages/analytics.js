8ac61d2ce876a58e51d4cec5e999acce7674c618
matanyaniv8 <matanyaniv8@gmail.com>
2024-07-22 18:39:29 +0300

    added more personal clicks label

--- system/pages/analytics.js
+++ system/pages/analytics.js
@@ -1,7 +1,7 @@
 // utils/analytics.js
 export const reportButtonClick = (buttonId, buttonLabel) => {
     if (window.gtag) {
-        window.gtag('event', `click`, {
+        window.gtag('event', `${buttonLabel} clicked`, {
             event_category: 'Button',
             event_label: buttonLabel,
             value: buttonId,
